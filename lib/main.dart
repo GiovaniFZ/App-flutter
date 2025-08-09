5a9cdd38f75a903fb622b69239c278b8ae296ac6
GiovaniFZ <giovanifinazzi@gmail.com>
2025-08-09 15:24:54 -0300

    chore: testing columns and containers

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Example', style: TextStyle(color: Colors.white)),
+          elevation: 1,
+          backgroundColor: Color(0xFF03378F),
+          leading: IconButton(
+            icon: Icon(Icons.menu, color: Colors.white),
+            onPressed: () {},
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.notifications, color: Colors.white),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.settings, color: Colors.white),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Expanded(child: Container(color: Colors.deepOrange[200])),
+            Expanded(child: Container(color: Colors.deepOrange[300])),
+            Expanded(child: Container(color: Colors.blue[100])),
+          ],
         ),
       ),
     );
