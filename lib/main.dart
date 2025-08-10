065c42ccee3a0ad981f864596e276a9f25c3a4b0
GiovaniFZ <giovanifinazzi@gmail.com>
2025-08-10 16:47:00 -0300

    style: change font to poppins

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/pages/login.dart';
 import 'package:flutter_app/pages/second.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MainApp());
@@ -13,6 +14,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
       home: Login(),
       routes: {
         '/login': (context) => Login(),
